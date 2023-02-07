f0f62f199abf15c96f5e1591c3487d487b1279c2
Alex Pitzel <pitzelalex@gmail.com>
2023-02-07 12:22:11 -0500

    feat: Create items api update endpoint

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -7,4 +7,18 @@ class Api::V1::ItemsController < ApplicationController
     item = Item.find(params[:id])
     render json: ItemSerializer.new(item)
   end
+
+  def create
+    render json: Item.create!(item_params)
+  end
+
+  def update
+    render json: Item.update(params[:id], item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -17,6 +17,8 @@ describe 'Items API' do
     items[:data].each do |i|
       expect(i).to have_key(:id)
       expect(i[:id]).to be_a String
+      expect(i).to have_key(:type)
+      expect(i[:type]).to be_a String
       expect(i).to have_key(:attributes)
       expect(i[:attributes]).to have_key(:name)
       expect(i[:attributes]).to have_key(:description)
@@ -32,7 +34,7 @@ describe 'Items API' do
 
   it 'sends a list of items' do
     item1 = create(:item)
-    item2 = create(:item)
+    create(:item)
 
     get "/api/v1/items/#{item1.id}"
 
@@ -44,6 +46,8 @@ describe 'Items API' do
     expect(item[:data]).to be_a Hash
     expect(item[:data]).to have_key(:id)
     expect(item[:data][:id]).to be_a String
+    expect(item[:data]).to have_key(:type)
+    expect(item[:data][:type]).to be_a String
     expect(item[:data]).to have_key(:attributes)
     expect(item[:data][:attributes]).to be_a Hash
     expect(item[:data][:attributes]).to have_key(:name)
@@ -55,5 +59,40 @@ describe 'Items API' do
     expect(item[:data][:attributes][:description]).to be_a String
     expect(item[:data][:attributes][:unit_price]).to be_a Float
     expect(item[:data][:attributes][:merchant_id]).to be_an Integer
+  end
+
+  it 'can create a new item' do
+    m = create(:merchant)
+    item_params = {
+      name: 'Super Awesome Item',
+      description: 'This item has a sick description. It truy is super awesome',
+      unit_price: 420.69,
+      merchant_id: m.id
+    }
+
+    headers = { 'CONTENT_TYPE' => 'application/json' }
+
+    post '/api/v1/items', headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last
+
+    expect(response).to be_successful
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+  end
+
+  it 'can update an existing item' do
+    id = create(:item).id
+    previous_name = Item.last.name
+    item_params = { name: 'Cool New Name' }
+    headers = { 'CONTENT_TYPE': 'application/json' }
+
+    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({ item: item_params })
+    item = Item.find(id)
+
+    expect(response).to be_successful
+    expect(item.name).not_to eq(previous_name)
+    expect(item.name).to eq('Cool New Name')
   end
 end
