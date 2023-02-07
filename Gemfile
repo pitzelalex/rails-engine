86ae81ee8f30de3e9fb101192fe041fc4b982a6e
Alex Pitzel <pitzelalex@gmail.com>
2023-02-06 19:18:19 -0500

    feat: Create serializer and complete test for get all merchants api call

--- Gemfile
+++ Gemfile
@@ -37,8 +37,9 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'factory_bot_rails'
-  gem 'rspec-rails', '~> 3.5'
   gem 'pry-rails'
+  gem 'rspec-rails', '~> 3.5'
+  gem 'jsonapi-serializer'
 end
 
 group :development do
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,6 +8,16 @@ describe 'Merchants API' do
 
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body)
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants).to have_key(:data)
+    expect(merchants[:data].count).to eq(3)
+
+    merchants[:data].each do |m|
+      expect(m).to have_key(:attributes)
+      expect(m[:attributes]).to have_key(:name)
+
+      expect(m[:attributes][:name]).to be_a String
+    end
   end
 end
