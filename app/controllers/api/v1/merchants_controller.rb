fffe995505c19bc0c3a8cd94edbcae933b10788e
Alex Pitzel <pitzelalex@gmail.com>
2023-02-06 19:33:07 -0500

    feat: Create get merchant by id api endpoint

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,4 +2,14 @@ class Api::V1::MerchantsController < ApplicationController
   def index
     render json: MerchantSerializer.new(Merchant.all)
   end
+
+  def show
+    render json: MerchantSerializer.new(merchant)
+  end
+
+  private
+
+  def merchant
+    Merchant.find(params[:id])
+  end
 end
