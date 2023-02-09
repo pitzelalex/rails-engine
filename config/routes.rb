762f1a3a32399ee04108a206fd8be02e883b2ead
Alex Pitzel <pitzelalex@gmail.com>
2023-02-09 15:22:13 -0500

    feat: Create sad paths for merchant find_all query"

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html\
   namespace :api do
     namespace :v1 do
-      get 'merchants/find', to: 'merchants/find#index'
+      get 'merchants/find_all', to: 'merchants/find#index'
       resources :merchants, only: %i[index show] do
         scope module: :merchants do
           resources 'items', only: %i[index]
--- spec/requests/api/v1/merchants/search_spec.rb
+++ spec/requests/api/v1/merchants/search_spec.rb
@@ -5,7 +5,7 @@ describe 'Merchants Search API' do
     merchant_names = ['Amazon', 'Apple', 'New Egg', 'Canada Computers', 'Best Buy', 'Intel', 'Advanced Micro Devices', 'Micro Center', 'Micron', 'Canadian Tire']
     merchants = merchant_names.map { |name| create(:merchant, name: name) }
 
-    get '/api/v1/merchants/find?name=mIcR'
+    get '/api/v1/merchants/find_all?name=mIcR'
 
     expect(response).to be_successful
 
@@ -17,5 +17,23 @@ describe 'Merchants Search API' do
     expect(found_merchants[:data][2][:id]).to eq(merchants[8].id.to_s)
     expect(found_merchants[:data][2][:type]).to eq('merchant')
     expect(found_merchants[:data][2][:attributes][:name]).to eq(merchants[8].name)
+  end
+
+  it 'requires valid search parameter' do
+    get '/api/v1/merchants/find_all'
+
+    expect(response).not_to be_successful
+
+    error = JSON.parse(response.body, symbolize_names: true)
+
+    expect(error[:errors][0][:message]).to eq('Must include 1 valid search parameter')
+
+    get '/api/v1/merchants/find_all?name'
+
+    expect(response).not_to be_successful
+
+    error = JSON.parse(response.body, symbolize_names: true)
+
+    expect(error[:errors][0][:message]).to eq("Name Can't be empty")
   end
 end
